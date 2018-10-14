357e0051a82857e5f7ec08189e1ed162b0afddd7
yikang <yikang@example.com>
2018-10-14 21:36:08 +0800

    从数据库拉取数据渲染到前端

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 var express = require('express');
-var nunjucks = require('nunjucks')
+var nunjucks = require('nunjucks');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '192.168.0.110',
+    user: 'root',
+    password: ' ',
+    database: 'okr'
+});
+
+connection.connect()
 
 var app = express();
 
-nunjucks.configure('views',{
+nunjucks.configure('views', {
     autoescape: true,
     express: app,
 });
 
-app.get('/homepage',function(req,res){
-    res.render('homepage.html')
+app.get('/homepage', function (req, res) {
+    connection.query('select * from okr', function (err, data) {
+        res.render('homepage.html', { okr: data });
+    })
+
 });
 
-app.get('/details',function(req,res){
+app.get('/details', function (req, res) {
     res.render('details.html')
 });
 
-app.get('/personal',function(req,res){
+app.get('/personal', function (req, res) {
     res.render('personal.html')
 });
 
